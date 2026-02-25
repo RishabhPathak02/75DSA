c6bba6c1e0bd5dd82ca4bf11f3ee8893a338b93e
Rishabh Pathak <rishabh06pathak@gmail.com>
2026-02-25 23:03:19 +0530

    feat:completed DSA day50 practice

---
+++ src/Day50/Sol.java
@@ -0,0 +1,30 @@
+package Day50;
+
+public class Sol {
+
+     public class TreeNode {
+              int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+    class Solution {
+        public int sumNumbers(TreeNode root) {
+            return helper(root, 0);
+        }
+        private int helper(TreeNode root,int curr) {
+            if (root == null)
+                return 0;
+            curr = curr * 10 + root.val;
+            if (root.left == null && root.right == null)
+                return curr;
+            return helper(root.left, curr) + helper(root.right, curr);
+        }
+    }
+}
